2c51d5a0670f95467409b8ecaec594ab65fb4470
Victoria Nicholson <saintvictoria@gmail.com>
2014-10-03 16:14:28 -0400

    boxes moving

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,14 @@
 console.log('The Iron Yard Rocks');
 
 
-$('.moving').click(function() {
-  console.log('click');
-   $('moving').removeClass('open');
-   $(this).addClass('open');
+// $('.moving').click(function() {
+//   console.log('click');
+//    $('section').removeClass('moving');
+//    $(this).addClass('section');
+// });
+
+$('h2').on('click',function(){
+  $('span').addClass('hide');
+  $(this).next().find('span').removeClass ('hide');
+  $(this).next().find('span').addClass('appear');
 });
